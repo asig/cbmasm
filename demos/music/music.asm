f1c63aba3c759420f1a39b55f8b87278231b1c36
Andreas Signer <asigner@gmail.com>
2021-05-22 21:48:17 +0200

    add license header

--- demos/music/music.asm
+++ demos/music/music.asm
@@ -1,3 +1,20 @@
+; Copyright (c) 2021 Andreas Signer <asigner@gmail.com>
+;
+; This file is part of cbmasm.
+;
+; cbmasm is free software: you can redistribute it and/or
+; modify it under the terms of the GNU General Public License as
+; published by the Free Software Foundation, either version 3 of the
+; License, or (at your option) any later version.
+;
+; cbmasm is distributed in the hope that it will be useful,
+; but WITHOUT ANY WARRANTY; without even the implied warranty of
+; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+; GNU General Public License for more details.
+;
+; You should have received a copy of the GNU General Public License
+; along with cbmasm.  If not, see <http://www.gnu.org/licenses/>.
+
 wait_line .macro line
         lda #line
 _l1     cmp $d012
@@ -31,5 +48,6 @@ loop
         jmp loop
 
         ; Use tools/sidconv to convert a sid file to asm source
-        .include "songdata.i"
+        .include "scapelands_4000.i"
+
 
