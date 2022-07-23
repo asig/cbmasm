c8807f68de53110a469742269d9a93a70d2d0b93
Andreas Signer <asigner@gmail.com>
2022-07-23 21:38:41 +0200

    Switched to ca65 to generate tests

--- tools/testgen.go
+++ tools/testgen.go
@@ -223,150 +223,154 @@ fd ae ee		   XOR (IY-$12)
 `
 
 var raw6502 = `
-0000        ADC $0056       6D 56 00
-0003        ADC $0078       6D 78 00
-0006        ADC $0078,X     7D 78 00
-0009        ADC $1234       6D 34 12
-000C        ADC $1234,X     7D 34 12
-000F        ADC $1234,Y     79 34 12
-0012        ADC ($9A,X)     61 9A
-0014        ADC ($BC),Y     71 BC
-0016        AND $0056       2D 56 00
-0019        AND $0078       2D 78 00
-001C        AND $0078,X     3D 78 00
-001F        AND $1234       2D 34 12
-0022        AND $1234,X     3D 34 12
-0025        AND $1234,Y     39 34 12
-0028        AND ($9A,X)     21 9A
-002A        AND ($BC),Y     31 BC
-002C        ASL $0078       0E 78 00
-002F        ASL $0078,X     1E 78 00
-0032        ASL $1234       0E 34 12
-0035        ASL $1234,X     1E 34 12
-0038        ASL A           0A
-0039        BIT $0078       2C 78 00
-003C        BIT $1234       2C 34 12
-003F        BRK             00
-0040        CLC             18
-0041        CLD             D8
-0042        CLI             58
-0043        CLV             B8
-0044        CMP $0056       CD 56 00
-0047        CMP $0078       CD 78 00
-004A        CMP $0078,X     DD 78 00
-004D        CMP $1234       CD 34 12
-0050        CMP $1234,X     DD 34 12
-0053        CMP $1234,Y     D9 34 12
-0056        CMP ($9A,X)     C1 9A
-0058        CMP ($BC),Y     D1 BC
-005A        CPX $0056       EC 56 00
-005D        CPX $0078       EC 78 00
-0060        CPX $1234       EC 34 12
-0063        CPY $0056       CC 56 00
-0066        CPY $0078       CC 78 00
-0069        CPY $1234       CC 34 12
-006C        DEC $0078       CE 78 00
-006F        DEC $0078,X     DE 78 00
-0072        DEC $1234       CE 34 12
-0075        DEC $1234,X     DE 34 12
-0078        DEX             CA
-0079        DEY             88
-007A        EOR $0056       4D 56 00
-007D        EOR $0078       4D 78 00
-0080        EOR $0078,X     5D 78 00
-0083        EOR $1234       4D 34 12
-0086        EOR $1234,X     5D 34 12
-0089        EOR $1234,Y     59 34 12
-008C        EOR ($9A,X)     41 9A
-008E        EOR ($BC),Y     51 BC
-0090        INC $0078       EE 78 00
-0093        INC $0078,X     FE 78 00
-0096        INC $1234       EE 34 12
-0099        INC $1234,X     FE 34 12
-009C        INX             E8
-009D        INY             C8
-009E        JMP $1234       4C 34 12
-00A1        JMP ($ABCD)     6C CD AB
-00A4        JSR $1234       20 34 12
-00A7        LDA $0056       AD 56 00
-00AA        LDA $0078       AD 78 00
-00AD        LDA $0078,X     BD 78 00
-00B0        LDA $1234       AD 34 12
-00B3        LDA $1234,X     BD 34 12
-00B6        LDA $1234,Y     B9 34 12
-00B9        LDA ($9A,X)     A1 9A
-00BB        LDA ($BC),Y     B1 BC
-00BD        LDX $0056       AE 56 00
-00C0        LDX $0078       AE 78 00
-00C3        LDX $0078,Y     BE 78 00
-00C6        LDX $1234       AE 34 12
-00C9        LDX $1234,Y     BE 34 12
-00CC        LDY $0056       AC 56 00
-00CF        LDY $0078       AC 78 00
-00D2        LDY $0078,X     BC 78 00
-00D5        LDY $1234       AC 34 12
-00D8        LDY $1234,X     BC 34 12
-00DB        LSR $0078       4E 78 00
-00DE        LSR $0078,X     5E 78 00
-00E1        LSR $1234       4E 34 12
-00E4        LSR $1234,X     5E 34 12
-00E7        LSR A           4A
-00E8        NOP             EA
-00E9        ORA $0056       0D 56 00
-00EC        ORA $0078       0D 78 00
-00EF        ORA $0078,X     1D 78 00
-00F2        ORA $1234       0D 34 12
-00F5        ORA $1234,X     1D 34 12
-00F8        ORA $1234,Y     19 34 12
-00FB        ORA ($9A,X)     01 9A
-00FD        ORA ($BC),Y     11 BC
-00FF        PHA             48
-0100        PHP             08
-0101        PLA             68
-0102        PLP             28
-0103        ROL $0078       2E 78 00
-0106        ROL $0078,X     3E 78 00
-0109        ROL $1234       2E 34 12
-010C        ROL $1234,X     3E 34 12
-010F        ROL A           2A
-0110        ROR $0078       6E 78 00
-0113        ROR $0078,X     7E 78 00
-0116        ROR $1234       6E 34 12
-0119        ROR $1234,X     7E 34 12
-011C        ROR A           6A
-011D        RTI             40
-011E        RTS             60
-011F        SBC $0056       ED 56 00
-0122        SBC $0078       ED 78 00
-0125        SBC $0078,X     FD 78 00
-0128        SBC $1234       ED 34 12
-012B        SBC $1234,X     FD 34 12
-012E        SBC $1234,Y     F9 34 12
-0131        SBC ($9A,X)     E1 9A
-0133        SBC ($BC),Y     F1 BC
-0135        SEC             38
-0136        SED             F8
-0137        SEI             78
-0138        STA $0078       8D 78 00
-013B        STA $0078,X     9D 78 00
-013E        STA $1234       8D 34 12
-0141        STA $1234,X     9D 34 12
-0144        STA $1234,Y     99 34 12
-0147        STA ($9A,X)     81 9A
-0149        STA ($BC),Y     91 BC
-014B        STX $0078       8E 78 00
-014E        STX $1234       8E 34 12
-0151        STX $78,Y       96 78
-0153        STY $0078       8C 78 00
-0156        STY $1234       8C 34 12
-0159        STY $78,X       94 78
-015B        TAX             AA
-015C        TAY             A8
-015D        TSX             BA
-015E        TXA             8A
-015F        TXS             9A
-0160        TYA             98
+ca65 V2.19 - Git e95db43
+Main file   : test6502.s
+Current file: test6502.s
+
+000000r 1  65 78                ADC $0078
+000002r 1  75 78                ADC $0078,X
+000004r 1  6D 34 12             ADC $1234
+000007r 1  7D 34 12             ADC $1234,X
+00000Ar 1  79 34 12             ADC $1234,Y
+00000Dr 1  61 9A                ADC ($9A,X)
+00000Fr 1  71 BC                ADC ($BC),Y
+000011r 1  25 56                AND $0056
+000013r 1  25 78                AND $0078
+000015r 1  35 78                AND $0078,X
+000017r 1  2D 34 12             AND $1234
+00001Ar 1  3D 34 12             AND $1234,X
+00001Dr 1  39 34 12             AND $1234,Y
+000020r 1  21 9A                AND ($9A,X)
+000022r 1  31 BC                AND ($BC),Y
+000024r 1  06 78                ASL $0078
+000026r 1  16 78                ASL $0078,X
+000028r 1  0E 34 12             ASL $1234
+00002Br 1  1E 34 12             ASL $1234,X
+00002Er 1  0A                   ASL A
+00002Fr 1  24 78                BIT $0078
+000031r 1  2C 34 12             BIT $1234
+000034r 1  00                   BRK
+000035r 1  18                   CLC
+000036r 1  D8                   CLD
+000037r 1  58                   CLI
+000038r 1  B8                   CLV
+000039r 1  C5 56                CMP $0056
+00003Br 1  C5 78                CMP $0078
+00003Dr 1  D5 78                CMP $0078,X
+00003Fr 1  CD 34 12             CMP $1234
+000042r 1  DD 34 12             CMP $1234,X
+000045r 1  D9 34 12             CMP $1234,Y
+000048r 1  C1 9A                CMP ($9A,X)
+00004Ar 1  D1 BC                CMP ($BC),Y
+00004Cr 1  E4 56                CPX $0056
+00004Er 1  E4 78                CPX $0078
+000050r 1  EC 34 12             CPX $1234
+000053r 1  C4 56                CPY $0056
+000055r 1  C4 78                CPY $0078
+000057r 1  CC 34 12             CPY $1234
+00005Ar 1  C6 78                DEC $0078
+00005Cr 1  D6 78                DEC $0078,X
+00005Er 1  CE 34 12             DEC $1234
+000061r 1  DE 34 12             DEC $1234,X
+000064r 1  CA                   DEX
+000065r 1  88                   DEY
+000066r 1  45 56                EOR $0056
+000068r 1  45 78                EOR $0078
+00006Ar 1  55 78                EOR $0078,X
+00006Cr 1  4D 34 12             EOR $1234
+00006Fr 1  5D 34 12             EOR $1234,X
+000072r 1  59 34 12             EOR $1234,Y
+000075r 1  41 9A                EOR ($9A,X)
+000077r 1  51 BC                EOR ($BC),Y
+000079r 1  E6 78                INC $0078
+00007Br 1  F6 78                INC $0078,X
+00007Dr 1  EE 34 12             INC $1234
+000080r 1  FE 34 12             INC $1234,X
+000083r 1  E8                   INX
+000084r 1  C8                   INY
+000085r 1  4C 34 12             JMP $1234
+000088r 1  6C CD AB             JMP ($ABCD)
+00008Br 1  20 34 12             JSR $1234
+00008Er 1  A5 56                LDA $0056
+000090r 1  A5 78                LDA $0078
+000092r 1  B5 78                LDA $0078,X
+000094r 1  AD 34 12             LDA $1234
+000097r 1  BD 34 12             LDA $1234,X
+00009Ar 1  B9 34 12             LDA $1234,Y
+00009Dr 1  A1 9A                LDA ($9A,X)
+00009Fr 1  B1 BC                LDA ($BC),Y
+0000A1r 1  A6 56                LDX $0056
+0000A3r 1  A6 78                LDX $0078
+0000A5r 1  B6 78                LDX $0078,Y
+0000A7r 1  AE 34 12             LDX $1234
+0000AAr 1  BE 34 12             LDX $1234,Y
+0000ADr 1  A4 56                LDY $0056
+0000AFr 1  A4 78                LDY $0078
+0000B1r 1  B4 78                LDY $0078,X
+0000B3r 1  AC 34 12             LDY $1234
+0000B6r 1  BC 34 12             LDY $1234,X
+0000B9r 1  46 78                LSR $0078
+0000BBr 1  56 78                LSR $0078,X
+0000BDr 1  4E 34 12             LSR $1234
+0000C0r 1  5E 34 12             LSR $1234,X
+0000C3r 1  4A                   LSR A
+0000C4r 1  EA                   NOP
+0000C5r 1  05 56                ORA $0056
+0000C7r 1  05 78                ORA $0078
+0000C9r 1  15 78                ORA $0078,X
+0000CBr 1  0D 34 12             ORA $1234
+0000CEr 1  1D 34 12             ORA $1234,X
+0000D1r 1  19 34 12             ORA $1234,Y
+0000D4r 1  01 9A                ORA ($9A,X)
+0000D6r 1  11 BC                ORA ($BC),Y
+0000D8r 1  48                   PHA
+0000D9r 1  08                   PHP
+0000DAr 1  68                   PLA
+0000DBr 1  28                   PLP
+0000DCr 1  26 78                ROL $0078
+0000DEr 1  36 78                ROL $0078,X
+0000E0r 1  2E 34 12             ROL $1234
+0000E3r 1  3E 34 12             ROL $1234,X
+0000E6r 1  2A                   ROL A
+0000E7r 1  66 78                ROR $0078
+0000E9r 1  76 78                ROR $0078,X
+0000EBr 1  6E 34 12             ROR $1234
+0000EEr 1  7E 34 12             ROR $1234,X
+0000F1r 1  6A                   ROR A
+0000F2r 1  40                   RTI
+0000F3r 1  60                   RTS
+0000F4r 1  E5 56                SBC $0056
+0000F6r 1  E5 78                SBC $0078
+0000F8r 1  F5 78                SBC $0078,X
+0000FAr 1  ED 34 12             SBC $1234
+0000FDr 1  FD 34 12             SBC $1234,X
+000100r 1  F9 34 12             SBC $1234,Y
+000103r 1  E1 9A                SBC ($9A,X)
+000105r 1  F1 BC                SBC ($BC),Y
+000107r 1  38                   SEC
+000108r 1  F8                   SED
+000109r 1  78                   SEI
+00010Ar 1  85 78                STA $0078
+00010Cr 1  95 78                STA $0078,X
+00010Er 1  8D 34 12             STA $1234
+000111r 1  9D 34 12             STA $1234,X
+000114r 1  99 34 12             STA $1234,Y
+000117r 1  81 9A                STA ($9A,X)
+000119r 1  91 BC                STA ($BC),Y
+00011Br 1  86 78                STX $0078
+00011Dr 1  8E 34 12             STX $1234
+000120r 1  96 78                STX $78,Y
+000122r 1  84 78                STY $0078
+000124r 1  8C 34 12             STY $1234
+000127r 1  94 78                STY $78,X
+000129r 1  AA                   TAX
+00012Ar 1  A8                   TAY
+00012Br 1  BA                   TSX
+00012Cr 1  8A                   TXA
+00012Dr 1  9A                   TXS
+00012Er 1  98                   TYA
 `
+
 func main() {
 	lines := strings.Split(rawZ80, "\n")
 	for _, l := range lines {
@@ -379,11 +383,10 @@ func main() {
 
 	fmt.Printf("-----------------------------------------------------------------\n")
 
-
 	lines = strings.Split(raw6502, "\n")
 	for _, l := range lines {
 		l := strings.TrimSpace(l)
-		if len(l) == 0 {
+		if !strings.HasPrefix(l, "00") {
 			continue
 		}
 		handleLine6502(strings.TrimSpace(l))
@@ -395,14 +398,13 @@ func handleLineZ80(line string) {
 	bytes := parts[0]
 	code := strings.TrimSpace(parts[len(parts)-1])
 
-
 	var wantBytes []string
-	for _,b := range strings.Split(bytes, " ") {
+	for _, b := range strings.Split(bytes, " ") {
 		wantBytes = append(wantBytes, fmt.Sprintf("0x%s", b))
 	}
 
 	fmt.Printf(
-	`		{
+		`		{
 			name: "Single instruction %s",
 			text: "%s",
 			want: []byte{%s},
@@ -410,13 +412,12 @@ func handleLineZ80(line string) {
 `, code, code, strings.Join(wantBytes, ", "))
 }
 
-
 func handleLine6502(line string) {
-	code := strings.TrimSpace(line[12:28])
-	bytes := strings.TrimSpace(line[28:])
+	bytes := strings.TrimSpace(line[11:32])
+	code := strings.TrimSpace(line[32:])
 
 	var wantBytes []string
-	for _,b := range strings.Split(bytes, " ") {
+	for _, b := range strings.Split(bytes, " ") {
 		wantBytes = append(wantBytes, fmt.Sprintf("0x%s", b))
 	}
 
